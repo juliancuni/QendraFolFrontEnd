e0051d7776c21ad2d84227376a498531a2d9e92a
Julian Cuni <juliancuni@gmail.com>
2021-04-29 20:34:15 +0200

    UserDto Updated

--- src/app/shared/sdk/models/user-dto.ts
+++ src/app/shared/sdk/models/user-dto.ts
@@ -1,6 +1,10 @@
 /* tslint:disable */
 /* eslint-disable */
 export interface UserDto {
+  email?: null | string;
+  emer?: null | string;
+  fotografiaUrl?: null | string;
+  mbiemer?: null | string;
   token?: null | string;
   userName?: null | string;
 }
