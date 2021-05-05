95fb3328ec84bb15e8373f95e779ac764b5f844d
Julian Cuni <juliancuni@gmail.com>
2021-05-05 09:49:08 +0200

    cleanup ngx-bootstrap

--- src/app/routes/home/home.module.ts
+++ src/app/routes/home/home.module.ts
@@ -1,14 +1,8 @@
 import { NgModule } from '@angular/core';
 import { HomeComponent } from './home/home.component';
 import { Routes, RouterModule } from '@angular/router';
-import { ModalModule } from 'ngx-bootstrap/modal';
 import { CommonModule } from '@angular/common';
-import { StoreModule } from '@ngrx/store';
-// import * as fromOldCeshtje from '../../store/reducers/old-ceshtje.reducer';
-import { EffectsModule } from '@ngrx/effects';
-// import { OldCeshtjeEffects } from '../../store/effects/old-ceshtje.effects';
 import { SharedModule } from 'src/app/shared/shared.module';
-import { OldCeshtjeDbEffects } from 'src/app/store/effects/old-ceshtje-db.effects';
 
 
 const routes: Routes = [
@@ -19,9 +13,6 @@ const routes: Routes = [
     imports: [
         CommonModule,
         RouterModule.forChild(routes),
-        ModalModule.forChild(),
-        // StoreModule.forFeature(fromOldCeshtje.oldCeshtjeFeatureKey, fromOldCeshtje.reducer),
-        // EffectsModule.forFeature([OldCeshtjeEffects]),
         SharedModule
     ],
     declarations: [HomeComponent],
